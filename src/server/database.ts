c4c4768f7e0d2639377572c971e30b3eac952f94
NickCoffin123 <nicholas.coffin@dcmail.ca>
2025-04-13 15:48:50 -0400

    trying to host

---
+++ src/server/database.ts
@@ -0,0 +1,52 @@
+"use strict"
+
+import { MongoClient, Db } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const USER_NAME = process.env.MONGO_USER;
+const PASSWORD = process.env.MONGO_PASSWORD;
+const DB_NAME = process.env.MONGO_DB;
+const CLUSTER= process.env.MONGO_CLUSTER;
+const MONG_URI = `mongodb+srv://${USER_NAME}:${PASSWORD}@${CLUSTER}.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+class Database {
+    private static instance: Database;
+    private client : MongoClient;
+    private db:Db | null = null;
+
+    private constructor() {
+        this.client = new MongoClient(MONG_URI);
+    }
+
+    public static getInstance() {
+        if (!Database.instance) {
+            Database.instance = new Database();
+        }
+        return Database.instance;
+    }
+
+    public async connect(): Promise<Db> {
+        if (!this.db) {
+            try {
+                await this.client.connect();
+                this.db = this.client.db(DB_NAME);
+                console.log("Connected to database");
+            } catch (error) {
+                console.log("Could not connect to database: " , error);
+                throw error;
+            }
+        }
+        return this.db;
+    }
+
+    public async disconnect(): Promise<void> {
+        await this.client.close();
+        console.log("Disconnected from database");
+        this.db = null;
+    }
+}
+
+export default Database;
